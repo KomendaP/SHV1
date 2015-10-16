e010dda65960d22e8c34967635bdb53b688c2f1a
Pavlo Komenda <Pavlo_Komenda@epam.com>
2015-10-16 19:43:22 +0300

    images and js

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,15 +1,15 @@
-module.exports = (function() {
-	$('.dd-1 ul.dropdown-menu > li').on('click', function (e) {
-		"use strict";
-		var $this = $(this);
-		$('#flag').find('span.flag').attr('class', $this.find('span.flag').attr('class'));
-		$('#flag').find('span.word').text($this.find('span.word').text());
-	});
+module.exports = (function($) {
+    $('.dd-1 ul.dropdown-menu > li').on('click', function (e) {
+        "use strict";
+        var $this = $(this);
+        $('#flag').find('span.flag').attr('class', $this.find('span.flag').attr('class'));
+        $('#flag').find('span.word').text($this.find('span.word').text());
+    });
 
-	$('.dd-2 ul.dropdown-menu > li').on('click', function (e) {
-		"use strict";
-		var $this = $(this);
-		var cur = $('#cur');
-		cur.find('span.cur').text($this.find('span.cur').text());
-	});
-})();
+    $('.dd-2 ul.dropdown-menu > li').on('click', function (e) {
+        "use strict";
+        var $this = $(this);
+        var cur = $('#cur');
+        cur.find('span.cur').text($this.find('span.cur').text());
+    });
+})($);
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,15 @@
-module.exports = (function() {
-	"use strict";
-	var slider = $('section.slider');
+module.exports = (function($) {
+    "use strict";
+    var slider = $('section.slider');
 
-	slider.find('li.slide.active').removeClass('.active');
-})();
+    slider.find('li.slide.active').removeClass('.active');
+    $.fn.beCarousel = function () {
+        var params = par || {},
+            slider = this;
+        
+        function init(self) {
+            this.slides = self.find('.slide')
+        }
+        return this;
+    }
+})($);
